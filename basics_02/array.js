2f1212f4ba8606173bcfcf64384a9432bd54858e
Rahul <rahuljethwa1411@gmail.com>
2024-05-15 01:28:28 +0530

    updated

--- basics_02/array.js
+++ basics_02/array.js
@@ -1,9 +1,16 @@
-const arr = [2,3,4,2,2]
-console.log(arr[2]);
+ const arr = [2,3,4,2,2]
+// console.log(arr[2]);
 
-const arr2 = new Array(2,3,4)
-console.log(arr2[1])
+ const arr2 = new Array(2,3,4)
+// console.log(arr2[1])
 
-arr.push(7)
-console.log(arr);
+ arr.push(7)
+// console.log(arr);
 
+
+// contactination
+const marvel = ["spiderman", "thor"]
+const dc = ["batman","superman"]
+
+const all = marvel.concat(dc)
+console.log(all)
