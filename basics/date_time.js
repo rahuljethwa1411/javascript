37c6e4c0b2eee7ce26673dcbc2e4814114027575
Rahul <rahuljethwa1411@gmail.com>
2024-05-14 23:55:17 +0530

    commit

---
+++ basics/date_time.js
@@ -0,0 +1,20 @@
+ let myDate = new Date()
+
+//  console.log(myDate);
+//  console.log(myDate.toLocaleString())
+//  console.log(myDate.toDateString())
+
+let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+
+// console.log(Math.floor(Date.now()/1000));     // to convert into seconds from milliseconds 
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1 );
+console.log(newDate.getDay());
