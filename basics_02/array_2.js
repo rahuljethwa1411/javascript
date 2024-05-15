2e2aac3caee67b60bb7fa9468b02edd7cd80806f
Rahul <rahuljethwa1411@gmail.com>
2024-05-15 22:08:53 +0530

    commit

---
+++ basics_02/array_2.js
@@ -0,0 +1,10 @@
+console.log(Array.isArray("rahul"))
+console.log(Array.from("rahul"))
+
+console.log(Array.from({name: "rahul"}))    // key or pair make an array so it'll show empty obj
+
+let score = 100;
+let score1 = 200;
+let score3 = 300;
+
+console.log(Array.of(score,score1,score3))
